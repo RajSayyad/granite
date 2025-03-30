6ee44e2d00e487f1537c816e7770e64640f6ffd0
RajSayyad <sayyadraj84@gmail.com>
2025-03-30 09:53:33 +0530

    Added slug

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,34 @@
 # frozen_string_literal: true
 
 class Task < ApplicationRecord
-  validates :title, presence: true
+  MAX_TITLE_LENGTH = 125
+  VALID_TITLE_REGEX = /\A.*[a-zA-Z0-9].*\z/i
+
+  validates :title,
+    presence: true,
+    length: { maximum: MAX_TITLE_LENGTH },
+    format: { with: VALID_TITLE_REGEX }
+  validates :slug, uniqueness: true
+  validate :slug_not_changed
+
+  before_create :set_slug
+
+  private
+
+    def set_slug
+      itr = 1
+      loop do
+        title_slug = title.parameterize
+        slug_candidate = itr > 1 ? "#{title_slug}-#{itr}" : title_slug
+        break self.slug = slug_candidate unless Task.exists?(slug: slug_candidate)
+
+        itr += 1
+      end
+    end
+
+    def slug_not_changed
+      if will_save_change_to_slug? && self.persisted?
+        errors.add(:slug, "is immutable!")
+      end
+    end
 end
