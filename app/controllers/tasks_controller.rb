4e70a10dfc6f33ab457937bdd29097337d64b79f
RajSayyad <sayyadraj84@gmail.com>
2025-04-02 16:50:53 +0530

    Added Signup feature

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -14,7 +14,7 @@ class TasksController < ApplicationController
   def create
     task = Task.new(task_params)
     task.save!
-    render_notice("Task was successfully created")
+    render_notice(t("successfully_created", entity: "Task"))
   end
 
   def update
