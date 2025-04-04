bdb6067b659f481f085ba961a2d223ac508382a1
RajSayyad <sayyadraj84@gmail.com>
2025-04-04 17:13:50 +0530

    Added service to customize task logger message.

--- test/jobs/task_logger_job_test.rb
+++ test/jobs/task_logger_job_test.rb
@@ -1,24 +1,13 @@
 # frozen_string_literal: true
 
-require "test_helper"
-require "sidekiq/testing"
+class TaskLoggerJob
+  include Sidekiq::Job
 
-class TaskLoggerJobTest < ActiveSupport::TestCase
-  def setup
-    @task = create(:task)
-  end
+  def perform(task_id)
+    task = Task.find(task_id)
+    message = LoggerMessageBuilderService.new(task).process!
 
-  def test_logger_runs_once_after_creating_a_task
-    Sidekiq::Testing.fake! do
-      assert_difference -> { TaskLoggerJob.jobs.size }, 1 do
-        TaskLoggerJob.perform_async(@task.id)
-      end
-    end
-  end
-
-  def test_log_count_increments_on_running_task_logger
-    assert_difference "Log.count", 1 do
-      TaskLoggerJob.new.perform(@task.id)
-    end
+    log = Log.create!(task_id: task.id, message:)
+    puts log.message
   end
 end
